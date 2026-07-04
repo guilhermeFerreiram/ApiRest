802b45fcb89f6bb2568ef67dc0eed03695932ad7
Guilherme Ferreira <guil.ferreiram@gmail.com>
2026-07-04 17:48:52 -0300

    feat: implement crud rules

--- APIRest/DTOs/ProductDto.cs
+++ APIRest/DTOs/ProductDto.cs
@@ -5,4 +5,7 @@ public class ProductDto
     public int Id { get; set; }
     public string Name { get; set; }
     public double Value { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
 }
--- APIRest/Services/ProductService.cs
+++ APIRest/Services/ProductService.cs
@@ -1,44 +1,112 @@
-﻿using APIRest.DTOs;
+﻿using System.Xml.Linq;
+using APIRest.Data;
+using APIRest.DTOs;
 using APIRest.Interfaces;
+using APIRest.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace APIRest.Services;
 
-public class ProductService : IProductService
+public class ProductService(AppDbContext context) : IProductService
 {
     public async Task<ProductDto> Create(string name, double value)
     {
-        return await Task.FromResult(new ProductDto { Id = 1, Name = "Computador", Value = 2000.00 });
+        var product = await context.Products
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Name == name && x.DeletedAt == null);
+
+        if (product is not null) throw new Exception("Já existe um produto com esse nome");
+
+        var productModel = new Product()
+        {
+            Name = name,
+            Value = value,
+            CreatedAt = DateTime.UtcNow, 
+        };
+
+        await context.Products.AddAsync(productModel);
+        await context.SaveChangesAsync();
+
+        var productDto = new ProductDto()
+        {
+            Id = productModel.Id,
+            Name = productModel.Name,
+            Value = productModel.Value,
+            CreatedAt = productModel.CreatedAt
+        };
+
+        return productDto;
     }
 
     public async Task Delete(int id)
     {
-        var product = await Get(id);
+        var product = await context.Products
+            .FirstOrDefaultAsync(x => x.Id == id && x.DeletedAt == null);
 
-        Console.WriteLine($"Produto deletado: {product.Name}");
+        if (product is null) throw new Exception($"Produto de id {id} não encontrado");
+
+        product.DeletedAt = DateTime.UtcNow;
+
+        await context.SaveChangesAsync();
     }
 
     public async Task<ProductDto> Get(int id)
     {
-        return await Task.FromResult(new ProductDto { Id = id, Name = "Computador", Value = 2000.00 });
+        var product = await context.Products
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id && x.DeletedAt == null);
+
+        if (product is null) throw new Exception($"Produto de id {id} não encontrado");
+
+        var productDto = new ProductDto()
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Value = product.Value,
+            CreatedAt = product.CreatedAt,
+            UpdatedAt = product.UpdatedAt
+        };
+
+        return productDto;
     }
 
     public async Task<List<ProductDto>> GetByFilters(List<int> ids, List<string> names)
     {
-        var products = new List<ProductDto>() { 
-            new() { Id = 1, Name = "Computador", Value = 2000.00 },
-            new() { Id = 2, Name = "Cadeira", Value = 300.00 },
-            new() { Id = 2, Name = "Mesa", Value = 500.00 },
-        };
-        return await Task.FromResult(products);
+        var query = context.Products.AsQueryable();
+
+        query = query.Where(x => x.DeletedAt == null);
+
+        if (ids is not null && ids.Count != 0)
+            query = query.Where(x => ids.Contains(x.Id));
+
+        if (names is not null && names.Count != 0)
+            query = query.Where(x => names.Contains(x.Name));
+
+        var products = await query.ToListAsync();
+
+        var productsDtos = products.Select(x => new ProductDto()
+        {
+            Id = x.Id,
+            Name = x.Name,
+            Value = x.Value,
+            CreatedAt = x.CreatedAt,
+            UpdatedAt = x.UpdatedAt
+        }).ToList();
+
+        return productsDtos;
     }
 
     public async Task Update(int id, string? name, double? value)
     {
-        var product = await Get(id);
+        var product = await context.Products
+            .FirstOrDefaultAsync(x => x.Id == id && x.DeletedAt == null);
+
+        if (product is null) throw new Exception($"Produto de id {id} não encontrado");
 
         product.Name = string.IsNullOrEmpty(name) ? product.Name : name;
         product.Value = value ?? product.Value;
+        product.UpdatedAt = DateTime.UtcNow;
 
-        Console.WriteLine($"Produto atualizado: {product.Name}");
+        await context.SaveChangesAsync();
     }
 }
