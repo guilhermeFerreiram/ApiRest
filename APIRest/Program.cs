1bda0f6db6ed36844c16509563a5500913e5926b
Guilherme Ferreira <guil.ferreiram@gmail.com>
2026-07-04 12:12:30 -0300

    feat: use swagger

--- APIRest/Program.cs
+++ APIRest/Program.cs
@@ -1,14 +1,26 @@
 using APIRest.Interfaces;
 using APIRest.Services;
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddScoped<IProductService, ProductService>();
 
+// versionamento de api
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Rest API", Version = "v1" });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("v1/swagger.json", "Rest API v1");
+});
 
 app.UseHttpsRedirection();
 
