56b5035b4c5bd7c107f1e44c3173523497743d39
Guilherme Ferreira <guil.ferreiram@gmail.com>
2026-07-04 17:04:51 -0300

    feat: add sqlite

---
+++ APIRest/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using APIRest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIRest.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- APIRest/Program.cs
+++ APIRest/Program.cs
@@ -1,20 +1,30 @@
+using APIRest.Data;
 using APIRest.Interfaces;
 using APIRest.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+var connectionString = builder.Configuration.GetConnectionString("SqliteConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+
 builder.Services.AddScoped<IProductService, ProductService>();
 
-// versionamento de api
 builder.Services.AddSwaggerGen(options =>
 {
     options.SwaggerDoc("v1", new OpenApiInfo { Title = "Rest API", Version = "v1" });
 });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+}
 
 app.UseSwagger();
 app.UseSwaggerUI(options =>
